2a6e78f7fbff29db45aed234c37ab0a41e11d24a
simaoppcastro <a31716216@fam.ulusiada.pt>
2020-05-07 13:17:53 +0100

    second commit

---
+++ StuffData/Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StuffData.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TelephoneNumber { get; set; }
+
+        // virtual keyword for lazy load
+        // public virtual RentStuffCard RentStuffCard { get; set; }
+    }
+}
---
+++ StuffData/StuffContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using StuffData.Models;
+
+namespace StuffData
+{
+    public class StuffContext : DbContext 
+    {
+        public StuffContext(DbContextOptions options) : base(options) { }
+        public DbSet<Client> Clients { get; set; }
+
+    }
+}
